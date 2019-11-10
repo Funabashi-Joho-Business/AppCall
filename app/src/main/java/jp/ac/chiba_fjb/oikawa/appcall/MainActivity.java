8a7cdceafba90f976df15129b8f1c4d86f0a05fb
SoraKumo <info@croud.jp>
2019-11-11 08:31:53 +0900

    ランタイムをAndroidXに更新

--- app/src/main/java/jp/ac/chiba_fjb/oikawa/appcall/MainActivity.java
+++ app/src/main/java/jp/ac/chiba_fjb/oikawa/appcall/MainActivity.java
@@ -2,13 +2,14 @@ package jp.ac.chiba_fjb.oikawa.appcall;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-// http://join/id=スプレッドシートのID
+//以下の呼び出し内容を表示する
+// http://join/id=パラメータ
 
 public class MainActivity extends AppCompatActivity {
 
